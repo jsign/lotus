bd618039f4456bcac61c4dffde07bf4098ea669e
Ignacio Hagopian <jsign.uy@gmail.com>
2020-07-31 10:33:07 -0300

    jsign: ipfs retrieval
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- node/node_test.go
+++ node/node_test.go
@@ -46,6 +46,7 @@ import (
 	"github.com/filecoin-project/lotus/miner"
 	"github.com/filecoin-project/lotus/node"
 	"github.com/filecoin-project/lotus/node/modules"
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	modtest "github.com/filecoin-project/lotus/node/modules/testing"
 	"github.com/filecoin-project/lotus/node/repo"
 	sectorstorage "github.com/filecoin-project/sector-storage"
@@ -379,7 +380,7 @@ func mockSbBuilder(t *testing.T, nFull int, storage []test.StorageMiner) ([]test
 		} else {
 			genesis = node.Override(new(modules.Genesis), modules.LoadGenesis(genbuf.Bytes()))
 		}
-
+		ipfsAddr := "/ip4/127.0.0.1/tcp/5001"
 		var err error
 		// TODO: Don't ignore stop
 		_, err = node.New(ctx,
@@ -390,6 +391,8 @@ func mockSbBuilder(t *testing.T, nFull int, storage []test.StorageMiner) ([]test
 			node.Test(),
 
 			node.Override(new(ffiwrapper.Verifier), mock.MockVerifier),
+			node.ApplyIf(func(s *node.Settings) bool { return true },
+				node.Override(new(dtypes.ClientBlockstore), modules.IpfsClientBlockstore(ipfsAddr, true))),
 
 			genesis,
 		)
